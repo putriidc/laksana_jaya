6198773b14983ff34c5010bcc4c70050e6225b00
AnwarMulyaIbrahim <anwaribrahima7x@gmail.com>
2026-01-26 21:36:32 +0700

    ad csrf on head

--- resources/views/kepala-gudang/layout.blade.php
+++ resources/views/kepala-gudang/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Admin</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite('resources/css/app.css') @vite('resources/js/app.js')
 </head>
 
