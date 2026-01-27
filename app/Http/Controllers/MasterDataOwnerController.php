641242649d867bb36d0b8ea0e2605ded99f31e9d
AnwarMulyaIbrahim <anwaribrahima7x@gmail.com>
2026-01-27 14:56:57 +0700

    fix master data saldo from jurnal

--- app/Http/Controllers/MasterDataOwnerController.php
+++ app/Http/Controllers/MasterDataOwnerController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Asset;
-use App\Models\Karyawan;
-use App\Models\PiutangHutang;
 use App\Models\Proyek;
+use App\Models\Karyawan;
 use App\Models\Supplier;
+use App\Models\JurnalUmum;
 use Illuminate\Http\Request;
+use App\Models\PiutangHutang;
 
 class MasterDataOwnerController extends Controller
 {
@@ -17,19 +18,25 @@ class MasterDataOwnerController extends Controller
     public function index()
     {
         $lancars = Asset::where('akun_header', 'asset_lancar')
-        ->whereNull('deleted_at')->get();
-        $banks = Asset::where('akun_header', 'asset_lancar_bank')
-        ->whereNull('deleted_at')->get();
+            ->whereNull('deleted_at')->get();
+        $banks = Asset::where('akun_header', 'asset_lancar_bank')->whereNull('deleted_at')->get()
+        ->map(function ($bank) {
+            $debit = JurnalUmum::where('kode_perkiraan', $bank->kode_akun)->sum('debit');
+            $kredit = JurnalUmum::where('kode_perkiraan', $bank->kode_akun)->sum('kredit');
+            $saldo =  $debit - $kredit;
+            $bank->saldo = $saldo;
+            return $bank;
+        });
         $tetaps = Asset::where('akun_header', 'asset_tetap')
-        ->whereNull('deleted_at')->get();
+            ->whereNull('deleted_at')->get();
         $kewajibans = Asset::where('akun_header', 'kewajiban')
-        ->whereNull('deleted_at')->get();
+            ->whereNull('deleted_at')->get();
         $ekuitass = Asset::where('akun_header', 'ekuitas')
-        ->whereNull('deleted_at')->get();
+            ->whereNull('deleted_at')->get();
         $pendapatans = Asset::where('akun_header', 'pendapatan')
-        ->whereNull('deleted_at')->get();
+            ->whereNull('deleted_at')->get();
         $hpps = Asset::where('akun_header', 'hpp_proyek')
-        ->whereNull('deleted_at')->get();
+            ->whereNull('deleted_at')->get();
         $piutangHutangs = PiutangHutang::whereNull('deleted_at')->get();
         $karyawans = Karyawan::whereNull('deleted_at')->get();
         $proyeks = Proyek::whereNull('deleted_at')->get();
